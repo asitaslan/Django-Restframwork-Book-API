e05d028a159cda510c30d0c44797f863d0e6f460
Aşıt Aslan <asitaslan95@outlook.com>
2021-08-30 14:26:12 +0300

    add and change books and comments

--- kitap_pazari/kitaplar/api/views.py
+++ kitap_pazari/kitaplar/api/views.py
@@ -1,19 +1,52 @@
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from kitaplar.api.serializers import KitapSerializer, YorumSerializer
-from kitaplar.models import  Kitap
+from kitaplar.models import  Kitap, Yorum
 
-class KitapListCreateAPIView(ListModelMixin, CreateModelMixin,  GenericAPIView):
+
+
+
+class KitapListCreateAPIView(generics.ListCreateAPIView):
     queryset =  Kitap.objects.all()
     serializer_class = KitapSerializer
-    #Listeleme
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+
+
+
+class KitapDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Kitap.objects.all()
+    serializer_class = KitapSerializer
+
+
+class YorumCreateAPIView(generics.CreateAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+    
+    def perform_create(self,serializer):
+
+        kitap_pk = self.kwargs.get('kitap_pk')
+        kitap = get_object_or_404(Kitap, pk=kitap_pk)
+        serializer.save(kitap=kitap)
+
+
+class YorumDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Yorum.objects.all()
+    serializer_class = YorumSerializer
+
+
+# class KitapListCreateAPIView(ListModelMixin, CreateModelMixin,  GenericAPIView):
+#    queryset =  Kitap.objects.all()
+#   serializer_class = KitapSerializer
+#    #Listeleme
+#   def get(self, request, *args, **kwargs):
+#        return self.list(request, *args, **kwargs)
+#    
+
+#   def post(self, request, *args, **kwargs):
+#      return self.create(request, *args, **kwargs)
 
 
 
